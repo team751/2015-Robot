4d6c09d189a2dd718f66fb22496411a95338d001
Sam Baumgarten <sambaumgarten98@gmail.com>
2015-10-03 01:09:13 -0700

    Add comments to the TotePosePacket struct

--- Lidar/LidarDataCommunication/inc/tote_pose_packet.h
+++ Lidar/LidarDataCommunication/inc/tote_pose_packet.h
@@ -12,20 +12,28 @@ struct TotePosePacket {
     double x;
     double y;
 
+    // Constructor
     ToteEndpoint(double x, double y) : x(x), y(y) {}
   };
   
-  ToteEndpoint firstEndpoint;
-  ToteEndpoint secondEndpoint;
+  ToteEndpoint firstEndpoint; // One of the line endpoints (order doesn't matter)
+  ToteEndpoint secondEndpoint; // The other line endpoint (order doesn't matter)
   double angle; // In radians
   
+  // Constructor
   TotePosePacket(ToteEndpoint firstEndpoint, ToteEndpoint secondEndpoint, double angle) : firstEndpoint(firstEndpoint), secondEndpoint(secondEndpoint), angle(angle) {}
   
+  // Returns a string in the tote format protocol form
   std::string formattedMessage() {
+    // Create a buffer to hold the message
     char message[100]; // 100 should be more than enough
+    
+    // Format the message
     sprintf(message, "%0.3f,%0.3f,%0.3f,%0.3f,%0.3f", firstEndpoint.x, firstEndpoint.y, secondEndpoint.x, secondEndpoint.y, angle);
     
+    // Convert the message to a std::string
     std::string messageString(message);
+    
     return messageString;
   }
 };
